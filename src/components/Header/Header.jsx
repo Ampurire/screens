09dbc1f40b72425cdd12e643295bf38616e33d71
ampurire joan <kiizajoan66@gmail.com>
2023-10-27 07:27:14 +0300

    menu bar commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <section className="h-wrapper">
+      <div className="h-container"> HOSTEL HAVEN</div>
+        <div className="h-menu">
+          <a 
+          href=""
+          >
+
+ 
+             HOME
+          </a>
+          <a href="">ABOUT</a>
+          <a href="">UPLOAD</a>
+          <a href="">LOG IN</a>
+          <a href="">SIGN UP</a>
+          
+        </div>
+    </section>
+   
+  )
+}
+
+export default Header
